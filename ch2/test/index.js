a2b0b4cd1c48060f3cc58442cce7e663b20de47a
kota oue <kota.oue@yappli.co.jp>
2020-08-15 07:50:47 +0900

    Update index.js

--- ch2/test/index.js
+++ ch2/test/index.js
@@ -2,19 +2,19 @@ const assert = require("assert");
 const fizzbuzz = require("../index");
 
 describe("fizzbuzz", () => {
-    if ("returns FizzBuzz when value is divisible by 15.", () => {
-        assert(fizzbuzz(30) === "FizzBuzz")
+    it("returns FizzBuzz when value is divisible by 15", () => {
+        assert(fizzbuzz(30) === "FizzBuzz");
     });
 
-    if ("returns Fizz when value is divisible by 3.", () => {
-        assert(fizzbuzz(9) === "Fizz")
+    it("returns Fizz when value is divisible by 3", () => {
+        assert(fizzbuzz(9) === "Fizz");
     });
 
-    if ("returns Buzz when value is divisible by 5.", () => {
-        assert(fizzbuzz(10) === "Buzz")
+    it("returns Buzz when value is divisible by 5", () => {
+        assert(fizzbuzz(10) === "Buzz");
     });
 
-    if ("returns the values when value is not divisible by 3 or 5.", () => {
-        assert(fizzbuzz(7) === "7")
+    it("returns the value when value is not divisible by 3 or 5", () => {
+        assert(fizzbuzz(7) === "7");
     });
 });
